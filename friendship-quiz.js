fcb4b34909657d53be3286a54c5c0c4932a77e0f
melodyzheng <zhengmelody99@gmail.com>
2017-07-21 14:37:15 -0700

    Update friendship-quiz.js

--- friendship-quiz.js
+++ friendship-quiz.js
@@ -12,14 +12,28 @@ window.onload = init;
 
 var answerList = []; //initialize answerList to be empty
 
-/* ADD YOUR CODE BELOW */
 
 //adds user-submitted answer to answerList
 function submitAnswer() {
+  var answer = document.getElementById("answerInput").value;
+  var lowercaseAnswer = answer.toLowerCase(); // changes all letters in answerInput to be lowercase
+  console.log(lowercaseAnswer);
+  answerList.push(lowercaseAnswer);
+  console.log(answerList);
 
 }
 
 //checks if user-submitted guess is in answerList
 function checkGuess() {
-
+  var guess = document.getElementById("guessInput").value;
+  var lowercaseGuess = guess.toLowerCase(); // changes all letters in guessInput to be lowercase
+  console.log(lowercaseGuess);
+  for(i=0;i<answerList.length;i++){
+      if (lowercaseGuess == answerList[i]){
+        alert("You have guessed correctly!");
+        return;
+      }
+  }
+  alert("Try again!");
 }
+
